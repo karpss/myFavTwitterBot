f2bcc781c1cfb63718ef745b964b8f0c61f85414
fam246 <ola.fam10@gmail.com>
2018-01-09 17:20:58 +0100

    Second commit

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const Twitter = require('twitter')
 const config = require('./config')
-const Client = new Twitter(config)
+const client = new Twitter(config)
 
 const parameters = {
   q: 'Buhari Atiku',
   lang: 'en',
   result_type: 'mixed',
-  count: 50
+  count: 3
 
 }
 
-Client.get('search/tweets', parameters, (err, data) => {
+client.get('search/tweets', parameters, (err, data) => {
   if (!err) {
     for (let i = 0; i < data.statuses.length; i++) {
       let tweet_id = {id: data.statuses[i].id_str}
-      Client.post('favorites/create', tweet_id, (err, response) => {
+      client.post('favorites/create', tweet_id, (err, response) => {
         if (err) {
           console.log(err[0].message)
         }
